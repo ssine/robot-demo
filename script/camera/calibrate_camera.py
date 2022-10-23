2093dadb67a4a287b5bbc6a729ff28103daa8eef
ssine <liu.siyao@qq.com>
2022-10-23 09:03:41 +0000

    feat: april detection result publication

--- script/camera/calibrate_camera.py
+++ script/camera/calibrate_camera.py
@@ -5,11 +5,14 @@ import glob
 
 w = 8
 h = 5
+square_size = 0.03  # 3cm
+
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(h,5,0)
 objp = np.zeros((h * w, 3), np.float32)
-objp[:, :2] = np.mgrid[0:w, 0:h].T.reshape(-1, 2)
+objp[:, :2] = np.mgrid[0:w, 0:h].T.reshape(-1, 2) * square_size
+print(objp)
 # Arrays to store object points and image points from all the images.
 objpoints = []  # 3d point in real world space
 imgpoints = []  # 2d points in image plane.
@@ -32,7 +35,7 @@ cv.destroyAllWindows()
 
 ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
-print(ret, mtx, dist)
+print(ret, mtx, dist, rvecs, tvecs)
 
 img = cv.imread(images[0])
 h, w = img.shape[:2]
--- src/april/april/april.py
+++ src/april/april/april.py
@@ -5,14 +5,14 @@ import rclpy
 from rclpy.node import Node
 from rclpy.qos import qos_profile_system_default
 from cv_bridge import CvBridge
-from sensor_msgs.msg import Image, CompressedImage
+from sensor_msgs.msg import Image
 from tf_transformations import quaternion_from_matrix
 from tf2_ros.transform_broadcaster import TransformBroadcaster
 from tf2_ros import TransformStamped
-from geometry_msgs.msg import Transform
+from geometry_msgs.msg import Pose, PoseArray
 from geometry_msgs.msg import Quaternion
 from geometry_msgs.msg import Vector3
-from std_msgs.msg import Header
+from interface.msg import AprilTagDetection, AprilTagDetectionArray
 
 options = apriltag.DetectorOptions(families='tag36h11')
 detector = apriltag.Detector(options)
@@ -30,6 +30,9 @@ class AprilDetectorNode(Node):
     self.detector = apriltag.Detector(apriltag.DetectorOptions(families='tag36h11'))
     self.caster = TransformBroadcaster(self)
     self.create_subscription(Image, '/camera_0', self.on_image, qos_profile_system_default)
+    self.pose_pub = self.create_publisher(PoseArray, '/april_poses', qos_profile_system_default)
+    self.det_pub = self.create_publisher(AprilTagDetectionArray, '/apriltag_detection_array',
+                                         qos_profile_system_default)
 
   def on_image(self, message: Image):
     image = self.bridge.imgmsg_to_cv2(message)
@@ -43,6 +46,9 @@ class AprilDetectorNode(Node):
     grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     detections = self.detector.detect(grey)
 
+    pose_msgs = []
+    detection_msgs = []
+
     for detection in detections:
       # calculate pose
       # tag size: whole picture in meters
@@ -55,10 +61,29 @@ class AprilDetectorNode(Node):
       tfs = TransformStamped()
       tfs.header.stamp = self.get_clock().now().to_msg()
       tfs.header.frame_id = 'camera_0'
-      tfs.child_frame_id = f'camera_0_t{detection.tag_id}'
+      tfs.child_frame_id = f'camera_0_t_{detection.tag_id}'
       tfs.transform.translation = Vector3(x=float(t[0]), y=float(t[1]), z=float(t[2]))
       tfs.transform.rotation = Quaternion(x=q[0], y=q[1], z=q[2], w=q[3])
       self.caster.sendTransform(tfs)
+
+      # publish pose and april detection results
+      pose = Pose()
+      pose.position.x = float(t[0])
+      pose.position.y = float(t[1])
+      pose.position.z = float(t[2])
+      pose.orientation = tfs.transform.rotation
+      pose_msgs.append(pose)
+
+      det = AprilTagDetection()
+      det.header = message.header
+      det.id = detection.tag_id
+      det.pose = pose
+      for i in range(4):
+        det.corners2d[i].x = detection.corners[i][0]
+        det.corners2d[i].y = detection.corners[i][1]
+      detection_msgs.append(det)
+    self.pose_pub.publish(PoseArray(header=message.header, poses=pose_msgs))
+    self.det_pub.publish(AprilTagDetectionArray(header=message.header, detections=detection_msgs))
 
 
 def main():
